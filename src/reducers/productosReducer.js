072f733c227c0002c411eda058120471aa32179a
KryptomSilver <abelromeror763@gmail.com>
2021-06-26 18:45:21 -0500

    Add Redux

---
+++ src/reducers/productosReducer.js
@@ -0,0 +1,12 @@
+// Cada reducer tiene su state
+const initialState = {
+    productos: [],
+    error: null,
+    loading: false,
+};
+export default function (state = initialState, action) {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
